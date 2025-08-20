b4d5f70975d7ae93d6253ea3cc5660147d363bc0
Bekhruz-Coder <bekhruzbek2022@gmail.com>
2025-08-20 23:29:44 +0900

    let variableName = variableValue; and console.log();

---
+++ 2_Values-Variables.js
@@ -0,0 +1,16 @@
+let name = "Bekhruzbek";
+console.log(name);
+
+let age = 22;
+console.log(age);
+
+let userName = 'username'; // variable type is: camelCase.
+console.log(userName);
+
+// let 1StartsWithNumberIsError = 'startsWithNumberIsError'; 
+
+// let other@SymbolUsed = 'otherSymbolUsed'; You can't use symbols in variable name, but you can use Dollar sign($);
+
+// let for = 'reservedWords'; You can't use reservedWors. like for or try.
+
+
