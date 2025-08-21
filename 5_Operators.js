43f1dd50a89c39ffce6a7159ca8a5baff3032a30
Bekhruz-Coder <bekhruzbek2022@gmail.com>
2025-08-21 23:21:51 +0900

    arithmetic/comparison/assignment operators.

---
+++ 5_Operators.js
@@ -0,0 +1,53 @@
+// arithmetic operators: +,-,*,/,%,++,--;
+let a = 5, b = 10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a++);
+console.log(a);
+console.log(++a);
+console.log(a);
+console.log(b--);
+console.log(b);
+console.log(--b);
+console.log(b);
+
+// comparison opeartors: ==, ===, !=, >, >=, <, <=;
+let i = 5, j = 10, k = "5";
+const z = i;
+
+console.log(i == k);
+console.log(i === k);
+console.log(i == z);
+console.log(i === z);
+console.log(i != j);
+console.log(i != z);
+console.log(i > j);
+console.log(i < j);
+console.log(i >= j);
+console.log(i <= j);
+
+
+// Assignment operators: =, +=, -=, *=, /=, %=;
+let d = 5, e = 10, c = 15;
+
+console.log(d);
+d = e;
+console.log(d);
+
+d += e;
+console.log(d);
+
+d -= e;
+console.log(d);
+
+c *= e;
+console.log(c);
+
+c /= 5;
+console.log(c);
+
+e %= 2;
+console.log(e); // 0
