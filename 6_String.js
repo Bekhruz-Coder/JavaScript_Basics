a97b0ffb6cbbacf699d1eb81a6b2c41fb4bea457
Bekhruz-Coder <bekhruzbek2022@gmail.com>
2025-08-21 23:37:07 +0900

    strings in js.

---
+++ 6_String.js
@@ -0,0 +1,15 @@
+let name = 'Ma\'murbek';
+console.log(name);
+
+let surname = "Ma'murov";
+console.log(surname);
+
+// Concatenation(birlashtirish) with "+" symbol.
+let introduction = "Hello my name is ";
+
+let message = introduction + name + "!.";
+console.log(message);
+
+// Template literals with: `etc ${variableName} etc`;
+let message1 = `Hello ${name}, You are welcome.`;
+console.log(message1);
