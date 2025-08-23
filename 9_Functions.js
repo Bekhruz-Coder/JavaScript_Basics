bfa2dbbd4cba1af361c96156bc62c4be1f8b9719
Bekhruz-Coder <bekhruzbek2022@gmail.com>
2025-08-23 23:59:16 +0900

    functions

---
+++ 9_Functions.js
@@ -0,0 +1,12 @@
+function myFunction(son1, son2) {
+    console.log(son1 + son2);
+    return son1 + son2; // returndan keyingi qatorda yoziladigan har qanday kod ishga tushmaydi.
+}
+
+myFunction(10,20);
+myFunction(40, 5);
+
+const result = myFunction(30,40);
+console.log(result + 15);
+
+
