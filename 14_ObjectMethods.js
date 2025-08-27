f3fd088a6f88e889542a0775e02c7b15bb68a9ca
Bekhruz-Coder <bekhruzbek2022@gmail.com>
2025-08-28 01:21:26 +0900

    Object methods, Object.keys / object.values / object.entries

---
+++ 14_ObjectMethods.js
@@ -0,0 +1,33 @@
+const programmer = {
+    name: 'Bruce',
+    age: 23,
+    greeting: function() {
+        console.log("Hello, My name is " + this.name);
+    }
+}
+
+console.log(programmer.greeting());
+
+
+// Object.keys()
+const programmerOne = {
+    name: 'Bruce',
+    age: 22
+}
+console.log(Object.keys(programmerOne));
+
+
+// Object.values()
+const programmerTwo = {
+    name: 'Bruce',
+    age: 22
+}
+console.log(Object.values(programmerTwo));
+
+
+// Object.entries() - array ko'rinishi
+const programmerThree = {
+    name: "Beck",
+    age: 32
+}
+console.log(Object.entries(programmerThree));
